d213f9f5fbec8faa2eed7fd500fe90c39f8ddf95
paulohfsantos <paulohfsantos1@gmail.com>
2023-07-31 19:00:25 -0300

    fix: AxiosAdapter

--- src/infra/AxiosAdapter.ts
+++ src/infra/AxiosAdapter.ts
@@ -8,19 +8,4 @@ export class AxiosAdapter {
     const response = await axios.get<T>(url);
     return this.exception.handleResponse(response);
   }
-
-  async post<T>(url: string, body: unknown): Promise<T> {
-    const response = await axios.post<T>(url, body);
-    return this.exception.handleResponse(response);
-  }
-
-  async put<T>(url: string, body: unknown): Promise<T> {
-    const response = await axios.put<T>(url, body);
-    return this.exception.handleResponse(response);
-  }
-
-  async delete<T>(url: string): Promise<T> {
-    const response = await axios.delete<T>(url);
-    return this.exception.handleResponse(response);
-  }
 }
